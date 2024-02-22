227d9ae0c5df6717bf564854f986e55570be8033
Giorgio <giorgio.miguel@skyapps.us>
2024-02-22 17:07:15 -0500

    Lab6_complete

--- lab_assignment_6.c
+++ lab_assignment_6.c
@@ -2,7 +2,21 @@
 
 int search(int numbers[], int low, int high, int value) 
 {
-	return -1;
+	int mid;
+	if (low > high)
+	{
+		return - 1;
+	}
+	mid = low + ((high-low) / 2);
+	if (numbers[mid] == value) 
+	{
+		return mid;
+	}
+	else if (numbers[mid] > value)
+			return search(numbers, low, mid - 1, value); 
+	else{
+		return search(numbers, mid + 1, high, value);
+	}
 }
 
 void printArray(int numbers[], int sz)
